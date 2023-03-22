23f1e9b5b6e7fb30baf74f192e5e74f9139da943
Emerson Franco <contao.emersonfranco@gmail.com>
2023-03-22 16:55:24 -0300

    move component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import { SectionComponent } from './components/section/section.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { FeedComponent } from './pages/feed/feed.component';
+import { UserProfileComponent } from './pages/profile/components/user-profile/user-profile.component';
 
 
 @NgModule({
