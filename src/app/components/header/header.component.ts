11fdd5dc2475c88420971345dc32a0e5bec7c722
Emerson Franco <contato.emersonfranco@gmail.com>
2023-04-05 18:45:07 -0300

    Bug paths

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,39 +1,28 @@
 import { Component, EventEmitter, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
-import { ModalUploadComponent } from '../modal-upload/modal-upload.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  // @ViewChild('modal') private modalContent: TemplateRef<ModalUploadComponent>
 
- 
-
-
-
+export class HeaderComponent implements OnInit {  
   IconPathSearch: any;
   IconPathUpload: any;
   IconPathMenu: any;
   IconPathLogo: any;
 
-
-
-  constructor() {
-
-  }
+  constructor() { }
 
   ngOnInit(): void {
     this.montarIcons()
 
   }
 
-
   montarIcons() {
-    this.IconPathSearch = 'assets/icons/procurar.png'
-    this.IconPathUpload = 'assets/icons/link.png'
-    this.IconPathMenu = 'assets/icons/menu.png'
+    this.IconPathSearch = './assets/icons/procurar.png'
+    this.IconPathUpload = './assets/icons/link.png'
+    this.IconPathMenu = './assets/icons/menu.png'
 
   }
 
@@ -48,8 +37,6 @@ export class HeaderComponent implements OnInit {
     const fade = document.querySelector('#fade')!;
     modal.classList.toggle('hide')
     fade.classList.toggle('hide')
-
-    
 
   }
 
